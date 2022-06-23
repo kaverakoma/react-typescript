3997000b8feb5738bfdbdb5eaf5094408767885e
DESKTOP-22K1I8M\WebLucas <lucas.souza@vinhasoft.com.br>
2022-06-23 13:43:24 -0300

    feat: Adicionado troca de tema e tema Dark ao projeto

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { useAppThemeContext } from "../shared/contexts/ThemeContext";
 
 export const AppRoutes = () => {
+  const { toggleTheme } = useAppThemeContext();
   return (
     <Routes>
-      <Route path="/home" element={<Button color="primary">TESTE</Button>} />
+      <Route path="/home" element={<Button color="primary" onClick={toggleTheme}>TESTE</Button>} />
       <Route path="*" element={<Navigate to={"/home"} />} />
     </Routes>
   );
